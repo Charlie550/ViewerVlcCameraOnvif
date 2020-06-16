0e6d72ff0988e2c0edaca16dd827154c48f52973
Juan Carlos Gómez Rangel <juan.gomez@contel.com.mx>
2020-06-16 15:34:53 -0500

    primer commit

---
+++ pruebaVLC/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace pruebaVLC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_ruta = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_captura = new System.Windows.Forms.Button();
+            this.btn_conectar = new System.Windows.Forms.Button();
+            this.txt_estado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txt_ruta
+            // 
+            this.txt_ruta.Location = new System.Drawing.Point(160, 334);
+            this.txt_ruta.Name = "txt_ruta";
+            this.txt_ruta.Size = new System.Drawing.Size(335, 20);
+            this.txt_ruta.TabIndex = 1;
+            this.txt_ruta.Text = "C:\\Users\\LIVE\\Desktop";
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(454, 25);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(146, 20);
+            this.txt_password.TabIndex = 2;
+            this.txt_password.Text = "Contel123.";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(250, 25);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(146, 20);
+            this.txt_user.TabIndex = 3;
+            this.txt_user.Text = "Contel";
+            // 
+            // txt_url
+            // 
+            this.txt_url.Location = new System.Drawing.Point(22, 25);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(181, 20);
+            this.txt_url.TabIndex = 4;
+            this.txt_url.Text = "172.16.0.155";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 121);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btn_captura
+            // 
+            this.btn_captura.Location = new System.Drawing.Point(501, 326);
+            this.btn_captura.Name = "btn_captura";
+            this.btn_captura.Size = new System.Drawing.Size(99, 28);
+            this.btn_captura.TabIndex = 6;
+            this.btn_captura.Text = "Tomar captura";
+            this.btn_captura.UseVisualStyleBackColor = true;
+            this.btn_captura.Click += new System.EventHandler(this.btn_captura_Click);
+            // 
+            // btn_conectar
+            // 
+            this.btn_conectar.Location = new System.Drawing.Point(22, 62);
+            this.btn_conectar.Name = "btn_conectar";
+            this.btn_conectar.Size = new System.Drawing.Size(120, 31);
+            this.btn_conectar.TabIndex = 7;
+            this.btn_conectar.Text = "Conectar";
+            this.btn_conectar.UseVisualStyleBackColor = true;
+            this.btn_conectar.Click += new System.EventHandler(this.btn_conectar_Click);
+            // 
+            // txt_estado
+            // 
+            this.txt_estado.Location = new System.Drawing.Point(22, 247);
+            this.txt_estado.Multiline = true;
+            this.txt_estado.Name = "txt_estado";
+            this.txt_estado.Size = new System.Drawing.Size(120, 59);
+            this.txt_estado.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Dirección IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ruta imagen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Usuario:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(451, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Contraseña:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(160, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 244);
+            this.panel1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 366);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_estado);
+            this.Controls.Add(this.btn_conectar);
+            this.Controls.Add(this.btn_captura);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_ruta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_ruta;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_captura;
+        private System.Windows.Forms.Button btn_conectar;
+        private System.Windows.Forms.TextBox txt_estado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ pruebaVLC/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.IO;
+using Vlc.DotNet.Forms;
+/*Agregar referencias a servicios:
+    https://www.onvif.org/ver10/device/wsdl/devicemgmt.wsdl (Devide)
+    https://www.onvif.org/ver20/media/wsdl/media.wsdl (Media)
+
+importar paquetes nuget
+    Vlc.DotNet.Forms
+    (Vlc.DotNet.Core y Vlc.DotNet.Core.Interops)
+
+copiar los dll en debug
+    axvlc.dll
+    libvlc.dll
+    libvlccore.dll
+    plugins(carpeta)
+*/
+
+namespace pruebaVLC
+{
+    public partial class Form1 : Form
+    {
+
+        VlcControl vlcPlayer;
+
+        UriBuilder deviceUri;
+        Media.Media2Client media;
+        Media.MediaProfile[] profiles;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            vlcPlayer = new VlcControl();
+
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+
+            var libDirectory = new DirectoryInfo(currentDirectory);
+            vlcPlayer.BeginInit();
+            vlcPlayer.VlcLibDirectory = libDirectory;
+            vlcPlayer.Dock = DockStyle.Fill;            
+            vlcPlayer.EndInit();
+            panel1.Controls.Add(vlcPlayer);
+        }
+
+
+        private void btn_conectar_Click(object sender, EventArgs e)
+        {
+            deviceUri = new UriBuilder("http:/onvif/device_service");
+
+            string[] addr = txt_url.Text.Split(':');
+            deviceUri.Host = addr[0];
+            if (addr.Length == 2)
+                deviceUri.Port = Convert.ToInt16(addr[1]);
+
+            System.ServiceModel.Channels.Binding binding;
+            HttpTransportBindingElement httpTransport = new HttpTransportBindingElement();
+            httpTransport.AuthenticationScheme = System.Net.AuthenticationSchemes.Digest;
+            binding = new CustomBinding(new TextMessageEncodingBindingElement(MessageVersion.Soap12WSAddressing10, Encoding.UTF8), httpTransport);
+
+            Device.DeviceClient device = new Device.DeviceClient(binding, new EndpointAddress(deviceUri.ToString()));
+
+            try
+            {
+                Device.Service[] services = device.GetServices(false);
+                Device.Service xmedia = services.FirstOrDefault(s => s.Namespace == "http://www.onvif.org/ver20/media/wsdl");
+                if (xmedia != null)
+                {
+                    media = new Media.Media2Client(binding, new EndpointAddress(xmedia.XAddr));
+                    media.ClientCredentials.HttpDigest.ClientCredential.UserName = txt_user.Text;
+                    media.ClientCredentials.HttpDigest.ClientCredential.Password = txt_password.Text;
+                    media.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+
+                    try
+                    {
+                        profiles = media.GetProfiles(null, null);
+                        if (profiles != null)
+                        {
+                            foreach (var p in profiles)
+                            {
+                                listBox1.Items.Add(p.Name);
+                            }
+                        }
+                    }
+                    catch (System.ServiceModel.ProtocolException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            catch (System.ServiceModel.EndpointNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_captura_Click(object sender, EventArgs e)
+        {
+            vlcPlayer.TakeSnapshot(txt_ruta.Text + "/img.jpg");
+        }      
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (profiles != null && listBox1.SelectedIndex >= 0)
+            {
+                UriBuilder uri = new UriBuilder(media.GetStreamUri("RtspOverHttp", profiles[listBox1.SelectedIndex].token));
+                uri.Host = deviceUri.Host;
+                uri.Port = deviceUri.Port;
+                uri.Scheme = "rtsp";
+                txt_estado.Text = uri.Path;
+
+                string[] options = { ":rtsp-http", ":rtsp-http-port=" + uri.Port, ":rtsp-user=" + txt_user.Text, ":rtsp-pwd=" + txt_password.Text, };
+
+
+
+                vlcPlayer.VlcMediaPlayer.Play(uri.Uri, options);
+
+            }
+        }
+
+       
+    }
+}
